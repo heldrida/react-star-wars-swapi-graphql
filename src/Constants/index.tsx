85d260f0adbbc5e5d0cc14e345c03340a63cf783
Helder Oliveira <heldrida@gmail.com>
2020-02-10 17:27:50 +0000

    updated constants

---
+++ src/Constants/index.tsx
@@ -0,0 +1,14 @@
+enum APPLICATION {
+  name = "nsPunkbitStarWarsTopTrumps",
+  defaultNumberOfPlayers = 2
+}
+
+enum PLAYER_CARD_TYPE {
+  people = "people",
+  starships = "starships"
+}
+
+export {
+  APPLICATION,
+  PLAYER_CARD_TYPE
+}
