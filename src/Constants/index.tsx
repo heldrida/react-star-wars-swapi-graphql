4d89dc769d07a1a938423c0f533ef4822b5525c4
Helder Oliveira <heldrida@gmail.com>
2020-02-10 17:07:28 +0000

    Created two constants as string ENUM for the Player Card Type people vs starships that will be used later

--- src/Constants/index.tsx
+++ src/Constants/index.tsx
@@ -1,1 +1,11 @@
-export const APP_NAME = "nsPunkbitStarWarsTopTrumps"
+const APP_NAME = "nsPunkbitStarWarsTopTrumps"
+
+enum PLAYER_CARD_TYPE {
+  PEOPLE = "people",
+  STARSHIPS = "starships"
+}
+
+export {
+  APP_NAME,
+  PLAYER_CARD_TYPE
+}
