95caa11787c8f69dd433b485b9bd202d7fdc322f
Helder Oliveira <heldrida@gmail.com>
2020-02-10 17:38:47 +0000

    Initialise the Apollo client; Add the Apollo provider to pass the client to react context api

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from "apollo-client"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: "http://localhost:8080"
+  })
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
