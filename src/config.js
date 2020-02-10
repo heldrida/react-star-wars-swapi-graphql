1795298c30d6dfd624d958caa39f3b827d024f79
Helder Oliveira <heldrida@gmail.com>
2020-02-10 18:57:01 +0000

    add theme colours for styled-components

--- src/config.js
+++ src/config.js
@@ -1,3 +1,29 @@
+
+const themeColors = {
+  default: {
+    schaussPink: "#FF93B9",
+    cottonCandy: "#FFB8D1",
+    pinkPearl: "#E4B4C2",
+    queenPink: "#E7CEE3",
+    platinum: "#E0E1E9",
+    lightCyan: "#DDFDFE",
+    lightBlue: "#6551FF",
+    darkGrey: "#222",
+    lightGrey: "#fff"
+  },
+  alternative: {
+    schaussPink: "#FF96BB",
+    cottonCandy: "#FFB8D1",
+    pinkPearl: "#E4B4C2",
+    queenPink: "#E7CEE3",
+    platinum: "#E0E1E9",
+    lightCyan: "#DDFDFE",
+    lightBlue: "#6551FF",
+    darkGrey: "#333",
+    lightGrey: "#fff"
+  }
+}
+
 export default {
   application: {
     name: "nsPunkbitStarWarsTopTrumps",
@@ -5,5 +31,6 @@ export default {
   },
   // Obs: If you are not running the API through the Docker container documented
   // in the README.md, use the alternative proxy's provided
-  graphqlEndpoint: process.env.NODE_ENV === 'development' ? "http://localhost:8080" : "https://swapi.apis.guru"
+  graphqlEndpoint: process.env.NODE_ENV === 'development' ? "http://localhost:8080" : "https://swapi.apis.guru",
+  themeColors
 }
