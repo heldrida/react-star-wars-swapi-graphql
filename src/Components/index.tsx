acf959b99da5ffea0e008d49f6c4dfcfc9c80d6b
Helder Oliveira <heldrida@gmail.com>
2020-02-10 23:43:22 +0000

    related to the preivous commit, attached remaining files

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,3 +1,4 @@
 export { default as NavigationBar } from './NavigationBar'
 export { default as BoxWrapper } from './BoxWrapper'
 export { default as NumberOfPlayerSelect } from './NumberOfPlayerSelect'
+export { default as UserOptionsMenu } from './UserOptionsMenu'
--- src/Containers/Home/index.tsx
+++ src/Containers/Home/index.tsx
@@ -1,39 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { NumberOfPlayerSelect } from '../../Components'
-import AppConfiguration from '../../config'
-import { TSelectOnChangeCallback } from '../../Types'
-
-const UserOptionsMenu = () => {
-  const initialUserInputOptionsState = {
-    playerType: '',
-    playerNames: {},
-    numberOfPlayers: AppConfiguration.application.defaultNumberOfPlayers
-  }
-  const [userInputOptions, setUserInputOptions] = useState(initialUserInputOptionsState)
-  const [numberOfPlayers, setNumberOfPlayers] = useState(AppConfiguration.application.defaultNumberOfPlayers)
-
-  const onChangeHandlerNumberOfPlayerSelect: TSelectOnChangeCallback = (e) => {
-    const { value } = e.target
-    setNumberOfPlayers(+value)
-    const data = Object.assign({
-      ...userInputOptions,
-      numberOfPlayers: +value
-    })
-    setUserInputOptions(data)
-  }
-
-  useEffect(() => {
-    console.log('[debug] <UserOptionsMenu>: userInputOptions: ', userInputOptions)
-  }, [userInputOptions])
-
-  useEffect(() => {
-    console.log('[debug] <UserOptionsMenu>: numberOfPlayers: ', numberOfPlayers)
-  }, [numberOfPlayers])
-
-  return (
-    <NumberOfPlayerSelect onChangeHandler={onChangeHandlerNumberOfPlayerSelect} numberOfPlayers={numberOfPlayers} />
-  )  
-}
+import React from 'react'
+import { UserOptionsMenu } from '../../Components'
 
 const Home = () => (
   <>
