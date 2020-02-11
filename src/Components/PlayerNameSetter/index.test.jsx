cbfc34aa0b736eafb3a855e7505e3a68df928ce2
Helder Oliveira <heldrida@gmail.com>
2020-02-11 16:04:58 +0000

    add test cases for player name setter

--- src/Components/PlayerNameSetter/index.test.jsx
+++ src/Components/PlayerNameSetter/index.test.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import PlayerNameSetter from './index'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
-test('should render a list of inputs equal to number of players', () => {
+describe('PlayerNameSetter', () => {
   const onChangeHandler = jest.fn(() => null)
   const numberOfPlayers = 6
   const { container } = render(<PlayerNameSetter onChangeHandler={onChangeHandler} numberOfPlayers={numberOfPlayers} />)
-  expect(container.querySelectorAll('input').length).toBe(numberOfPlayers)
+  const userInputData = 'John Michael'
+  const userInputChars = [...userInputData]
+  const input = container.querySelector('input[name="Player5"]')
+  
+  // events
+  userInputChars.reduce((acc, curr) =>{
+    acc += curr
+    fireEvent.change(input, { target: { value: acc } })
+    return acc
+  }, "")
+
+  it('should render a list of inputs equal to number of players', () => {
+    expect(container.querySelectorAll('input').length).toBe(numberOfPlayers)
+  })
+  
+  it('should display the user input', () => {
+    expect(input.value).toBe(userInputData)  
+  })
+
+  it('should call the onChange handler per each user typed character', () => {
+    expect(onChangeHandler.mock.calls.length).toBe(userInputChars.length)
+  })
+
 })
